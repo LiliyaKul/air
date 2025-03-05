bf676c305a65ea26d6da2a3d37fec3c692e9a91c
Liliya K <Lilyusha-kul@yandex.ru>
2025-03-05 15:59:46 +0300

    Refactoring

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,19 +1,23 @@
-const menu = document.getElementById('burger-menu');
-const menuClose = document.getElementById('burger-menu-close');
-const menuOpen = document.getElementById('burger-menu-open');
 
-if (menuOpen) {
-    menuOpen.addEventListener('click', function() {
-        if (menu) {
-            menu.setAttribute('data-open', '');
-        }
-    });
-}
+(function() {
+    const menu = document.getElementById('burger-menu');
+    const menuClose = document.getElementById('burger-menu-close');
+    const menuOpen = document.getElementById('burger-menu-open');
 
-if (menuClose) {
-    menuClose.addEventListener('click', function() {
-        if (menu) {
-            menu.removeAttribute('data-open');
-        }
-    });
+    if (menuOpen) {
+        menuOpen.addEventListener('click', function() {
+            console.log(menu);
+            if (menu) {
+                menu.setAttribute('data-open', '');
+            }
+        });
+    }
+
+    if (menuClose) {
+        menuClose.addEventListener('click', function() {
+            if (menu) {
+                menu.removeAttribute('data-open');
+            }
+        });
 }
+})();
